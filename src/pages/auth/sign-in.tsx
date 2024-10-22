166acf0d226f05904026bec33c81094e234aab9c
Nathan Baldez <nathanbsb2@hotmail.com>
2024-10-21 21:11:39 -0300

    configs: react helmet (seo)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export default function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <h1>SignIn</h1>
+    </>
+  )
 }
