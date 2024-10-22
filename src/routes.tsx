973b11ec077aa8432930e99e3340aa0b1e39bf6e
Nathan Baldez <nathanbsb2@hotmail.com>
2024-10-21 21:07:20 -0300

    feature: rotas iniciais da aplicação

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import AppLayout from './pages/__layouts/app'
+import AuthLayout from './pages/__layouts/auth'
 import Dashboard from './pages/app/dashboard'
 import SignIn from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
   },
   {
-    path: 'sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
   },
 ])
