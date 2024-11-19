cdf8573a710f043e18a8fd61298e6d681f308716
Nathan Baldez <nathanbsb2@hotmail.com>
2024-11-19 07:36:51 -0300

    feature: conexão com bd

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,12 +1,14 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { toast } from 'sonner'
-import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -15,21 +17,28 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export default function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting} } = useForm<SignInForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
 
   async function handleSignIn(data: SignInForm) {
     try {
       console.log(data)
 
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-  
+      await authenticate({ email: data.email })
+
       toast.success('Enviamos um link de autenticação para seu e-mail', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data)
+          onClick: () => handleSignIn(data),
         },
       })
-
     } catch (error) {
       toast.error('Credenciais inválidas!')
     }
@@ -39,17 +48,10 @@ export default function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
-
-        <Button 
-          variant="ghost" 
-          asChild 
-          className="absolute right-8 top-8"
-        >
-          <Link to="/sign-up">
-            Novo estabelecimento
-          </Link>
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
         </Button>
-        
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
